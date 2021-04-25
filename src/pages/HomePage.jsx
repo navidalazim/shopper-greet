0cb4bad8a3fa642251fd3043b0ea7fa1d9a15da3
IbtehazC <ibtehaz.chowdhury@gmail.com>
2021-04-26 05:17:37 +0600

    Revert "Revert "Drawer Update with no funtionalities""
    This reverts commit 83018ed24109c93a07425361da10d7a92952ec79.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,20 @@
-import React from "react";
+import React, { Profiler } from "react";
 import { Link } from "react-router-dom";
 import "./homepage.css";
+import { useState } from "react";
 
 export default function HomePage({ products }) {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <>
       <h1>Categories</h1>
       <div className="categories">
         {products.map((product) => (
           <div className="category-card" key={product.category}>
-            <img
-              src={product.image}
-              alt={product.category}
-            />
-            <Link to={`/category/${product.categoryId}`}>{product.category}</Link>
+            <img src={product.image} alt={product.category} />
+            <Link to={`/category/${product.categoryId}`}>
+              {product.category}
+            </Link>
           </div>
         ))}
       </div>
